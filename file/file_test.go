4b61d57ec5789122d7b8fdc1a90e9b7da31206a4
Andreas Atle <atle.andreas@gmail.com>
2020-09-18 15:45:11 -0500

    daily-17 longest path

---
+++ file/file_test.go
@@ -0,0 +1,15 @@
+package file_test
+
+import (
+	"testing"
+
+	"github.com/andreasatle/Assorted/Daily/file"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLruCache(t *testing.T) {
+	file1 := "dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext"
+	file2 := "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"
+	assert.Equal(t, 20, file.GetMaxPathLength(file1), "Wrong length of largest path.")
+	assert.Equal(t, 32, file.GetMaxPathLength(file2), "Wrong length of largest path.")
+}
