d27ea72445bdfce64ff7a3e33a7e308462c1b055
Andreas Atle <atle.andreas@gmail.com>
2020-09-22 20:09:53 -0500

    daily-3 binary tree stringer.

--- strtree/strtree.go
+++ strtree/strtree.go
@@ -2,7 +2,6 @@
 // This problem was asked by Google.
 // Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, and deserialize(s), which deserializes the string back into the tree.
 // For example, given the following Node class
-
 // class Node:
 //    def __init__(self, val, left=None, right=None):
 //        self.val = val
@@ -13,12 +12,71 @@
 // assert deserialize(serialize(node)).left.left.val == 'left.left'
 package strtree
 
+import (
+	"strings"
+)
+
+// Node contains one node of a binary tree
 type Node struct {
 	Value string
 	Left  *Node
 	Right *Node
 }
 
+// Serialize is a stringer for a binary tree
 func (n *Node) Serialize() string {
-	return ""
+	if n == nil {
+		return "-"
+	}
+	return "Node(" + n.Value + "," + n.Left.Serialize() + "," + n.Right.Serialize() + ")"
+}
+
+// Deserialize is a string to binary tree converter
+func Deserialize(str string) *Node {
+	if strings.HasPrefix(str, "-") {
+		return nil
+	}
+	if !strings.HasPrefix(str, "Node(") {
+		panic("Wrong prefix")
+	}
+
+	findLeftRightSplit := func(str string) int {
+		split, count := 0, 0
+		for i, c := range str {
+			if c == '(' {
+				count++
+			} else if c == ')' {
+				count--
+			} else if c == ',' && count == 0 {
+				split = i
+			}
+		}
+		return split
+	}
+
+	split := strings.Index(str, ",")
+	val := str[5:split]
+	stripped := str[split+1 : len(str)-1]
+
+	split = findLeftRightSplit(stripped)
+	left := stripped[:split]
+	right := stripped[split+1:]
+	return &Node{val, Deserialize(left), Deserialize(right)}
+}
+
+// Cmp returns true if n and n2 are equal.
+func (n *Node) Cmp(n2 *Node) bool {
+	if n == nil && n2 == nil {
+		return true
+	}
+
+	if (n == nil && n2 != nil) || (n != nil && n2 == nil) {
+		return false
+	}
+
+	if n.Value != n2.Value {
+		return false
+	}
+
+	return n.Left.Cmp(n2.Left) && n.Right.Cmp(n2.Right)
 }
--- strtree/strtree_test.go
+++ strtree/strtree_test.go
@@ -8,7 +8,20 @@ import (
 )
 
 func TestTree(t *testing.T) {
-	tree := &strtree.Node{"root", nil, nil}
-	_ = tree
-	assert.Equal(t, true, true)
+	var tree, tree2, tree3 *strtree.Node
+
+	tree = &strtree.Node{"root", &strtree.Node{"root->left", nil, nil}, &strtree.Node{"root->right", nil, &strtree.Node{"root->right->right", nil, nil}}}
+	tree2 = strtree.Deserialize(tree.Serialize())
+	tree3 = &strtree.Node{"root", &strtree.Node{"root->left", nil, nil}, &strtree.Node{"root->right", nil, &strtree.Node{"root->right->righ", nil, nil}}}
+
+	// Check Cmp function
+	assert.Equal(t, false, tree.Cmp(tree3))
+	assert.Equal(t, true, tree.Cmp(tree))
+
+	// Check Serialize and Deserialize
+	assert.Equal(t, true, tree.Cmp(tree2))
+
+	tree = nil
+	tree2 = strtree.Deserialize(tree.Serialize())
+	assert.Equal(t, true, tree.Cmp(tree2))
 }
