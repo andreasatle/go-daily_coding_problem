95812f964aadc154b6cb9726d1f582a8120663d3
Andreas Atle <atle.andreas@gmail.com>
2020-10-14 12:32:20 -0500

    daily-8 Improved test coverage.

--- unival/unival_test.go
+++ unival/unival_test.go
@@ -11,3 +11,8 @@ func TestUnival(t *testing.T) {
 	tree := &unival.Node{0, &unival.Node{1, nil, nil}, &unival.Node{0, &unival.Node{1, &unival.Node{1, nil, nil}, &unival.Node{1, nil, nil}}, &unival.Node{0, nil, nil}}}
 	assert.Equal(t, 5, tree.Count())
 }
+
+func TestUnivalEmpty(t *testing.T) {
+	var tree *unival.Node
+	assert.Equal(t, 0, tree.Count())
+}
