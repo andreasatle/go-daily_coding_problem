52fe7db4b648711139151bd1fada6f2dafbe614d
Andreas Atle <atle.andreas@gmail.com>
2020-10-13 23:09:53 -0500

    daily-102 Improve test coverage.

--- cont/cont_test.go
+++ cont/cont_test.go
@@ -8,8 +8,19 @@ import (
 )
 
 func TestCont(t *testing.T) {
-	terms := cont.FindTerms([]int{2, 3, 6, 9, -3, 4, 1}, 10)
+	var terms []int
+
+	// Working case
+	terms = cont.FindTerms([]int{2, 3, 6, 9, -3, 4, 1}, 10)
 	assert.Equal(t, 9, terms[0])
 	assert.Equal(t, -3, terms[1])
 	assert.Equal(t, 4, terms[2])
+
+	// Empty case
+	terms = cont.FindTerms([]int{}, 10)
+	assert.Equal(t, 0, len(terms))
+
+	// Not working case
+	terms = cont.FindTerms([]int{}, 10)
+	assert.Equal(t, 0, len(terms))
 }
